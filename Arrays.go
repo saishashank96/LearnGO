0d1adcbebd33d2ded08e15921e83195da4da52fd
Saishashank Konduri <kondurisaishashank@gmail.com>
2023-02-26 11:33:27 -0600

    Array slices

---
+++ Arrays.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	//"bufio"
+	"fmt"
+	//"math"
+	//"os"
+	//"strconv"
+//	"strings"
+)
+
+
+func main() {
+	var color =[]string{"Green"}
+	color[0]="Red"
+	color=append(color,"Blue")
+	
+	fmt.Print(color)
+
+
+}
